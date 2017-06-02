4de17b33e25d1ef9168f7fd58e20372ffae9ccd8
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-02 02:11:41 -0400

    created possible timer function in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,13 @@ $(document).ready(function(){
 //questions are hidden when page loads
 	$("#triviaForm").hide();
 //to begin game
-	
+	function start() {
+		alert ("You've Started the game")
+		console.log ("Trial")
 
+
+
+	}
 
 
 
@@ -13,3 +18,20 @@ $(document).ready(function(){
 
 
 });
+//variables
+var correct 	= "0"
+var incorrect 	= "0"
+var unanswered 	= "0"
+
+//countdown clock function
+	var interval = setInterval(function(){
+		var counter = 30;
+		setInterval(function(){
+			counter --;
+			if(counter==0){
+				clearInterval(interval);
+			}
+			console.log('clock', counter);
+		}, 1 x 1000;
+	}
+
